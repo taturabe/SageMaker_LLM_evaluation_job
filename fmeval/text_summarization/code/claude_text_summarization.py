d39908784a213795a78924efd036f2bf7e00e346
taturabe <taturabe@amazon.co.jp>
2024-04-11 13:39:52 +0000

    create pipeline

--- fmeval/text_summarization/code/claude_text_summarization.py
+++ fmeval/text_summarization/code/claude_text_summarization.py
@@ -1,14 +1,14 @@
 import os
 import json
+import argparse
 
 from fmeval.data_loaders.data_config import DataConfig
 from fmeval.model_runners.bedrock_model_runner import BedrockModelRunner
 from fmeval.constants import MIME_TYPE_JSONLINES
 from fmeval.eval_algorithms.summarization_accuracy import SummarizationAccuracy
 
-
-input_data_path = os.path.join("/opt/ml/processing/input/data", "xsum_sample.jsonl")
-
+import boto3
+print(boto3.__version__)
 
 try:
     os.makedirs("/opt/ml/processing/output/evaluation")
@@ -19,6 +19,21 @@ except Exception as e:
     print("Could not make directories")
     pass
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input_data', type=str, required=True, help='filename placed in ProcessingInput')
+parser.add_argument('--model_id', type=str, required=True, help='model id')
+#parser.add_argument('--region', type=str, required=True, help='region')
+
+
+args = parser.parse_args()
+
+
+
+input_data_path = os.path.join("/opt/ml/processing/input/data", args.input_data)
+
+
+
 config = DataConfig(
     dataset_name="xsum_sample",
     dataset_uri=input_data_path,
@@ -28,7 +43,7 @@ config = DataConfig(
 )
 
 bedrock_model_runner = BedrockModelRunner(
-    model_id='anthropic.claude-v2',
+    model_id=args.model_id,
     output='completion',
     content_template='{"prompt": $prompt, "max_tokens_to_sample": 500}'
 )
--- fmeval/text_summarization/code/openai_text_summarization.py
+++ fmeval/text_summarization/code/openai_text_summarization.py
@@ -1,22 +1,36 @@
 import os
 import json
-
+import argparse
 from fmeval.data_loaders.data_config import DataConfig
-from fmeval.model_runners.bedrock_model_runner import BedrockModelRunner
+from fmeval.model_runners.model_runner import ModelRunner
+
 from fmeval.constants import MIME_TYPE_JSONLINES
 from fmeval.eval_algorithms.summarization_accuracy import SummarizationAccuracy
 import boto3
 import base64
 from botocore.exceptions import ClientError
+import requests
+from dataclasses import dataclass
+from typing import Tuple, Optional
 
+from fmeval.model_runners.model_runner import ModelRunner
 
 SECRET_NAME="OPENAI-API-KEY"
 KEY_NAME = "openai_api_key"
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input_data', type=str, required=True, help='filename placed in ProcessingInput')
+parser.add_argument('--model_id', type=str, required=True, help='model id')
+
+
+args = parser.parse_args()
+
+
 def get_secret():
     session = boto3.session.Session()
     client = session.client(
-        service_name='secretsmanager'
+        service_name='secretsmanager',
     )
 
     try:
@@ -57,7 +71,6 @@ class ChatGPTModelConfig:
     max_tokens: int
     api_key: str
 
-
 class ChatGPTModelRunner(ModelRunner):
     url = "https://api.openai.com/v1/chat/completions"
     
@@ -66,7 +79,7 @@ class ChatGPTModelRunner(ModelRunner):
         
     def predict(self, prompt: str) -> Tuple[Optional[str], Optional[float]]:
         payload = json.dumps({
-            "model": "gpt-4",
+            "model": args.model_id,
             "messages": [
                 {
                     "role": "user",
@@ -88,11 +101,17 @@ class ChatGPTModelRunner(ModelRunner):
         }
         
         response = requests.request("POST", self.url, headers=headers, data=payload)
+        print("#################")
+        print(response.text)
         return json.loads(response.text)["choices"][0]["message"]["content"], None
 
 
 
-input_data_path = os.path.join("/opt/ml/processing/input/data", "xsum_sample.jsonl")
+
+
+input_data_path = os.path.join("/opt/ml/processing/input/data", args.input_data)
+
+
 
 
 try:
@@ -104,7 +123,7 @@ except Exception as e:
     print("Could not make directories")
     pass
 
-config = DataConfig(
+data_config = DataConfig(
     dataset_name="xsum_sample",
     dataset_uri=input_data_path,
     dataset_mime_type=MIME_TYPE_JSONLINES,
@@ -114,17 +133,17 @@ config = DataConfig(
 
 
 # Note: Don't forget to include your api_key
-config = ChatGPTModelConfig(
-	api_key= get_secret()[KEY_NAME],
+model_config = ChatGPTModelConfig(
+	api_key= json.loads(get_secret())[KEY_NAME],
 	temperature=1.0,
 	top_p=1.0,
 	max_tokens=500
 )
 
-chat_gpt_model_runner = ChatGPTModelRunner(config)
+chat_gpt_model_runner = ChatGPTModelRunner(model_config)
 eval_algo = SummarizationAccuracy()
 
-eval_output = eval_algo.evaluate(model=chat_gpt_model_runner, dataset_config=config, 
+eval_output = eval_algo.evaluate(model=chat_gpt_model_runner, dataset_config=data_config, 
                                  prompt_template="Human: Summarise the following text in one sentence: $model_input\n\nAssistant:\n", save=True)
 
 with open("opt/ml/processing/output/evaluation/eval_res.json", "w") as f:
